d86572533c6d2d93af448ee8e81dc6dcf77309d1
WeisonWei <weixx3@126.com>
2020-03-27 21:04:15 +0800

    init

---
+++ src/test/java/com/es/gateway/api/ESchoolServiceGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.gateway.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESchoolServiceGatewayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
